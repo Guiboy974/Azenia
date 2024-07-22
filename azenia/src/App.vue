e4d744ee46edf716902d341836ea66366168ed57
Guillaume Boyer <guiboy97.410@gmail.com>
2024-07-22 11:27:48 +0200

    install vue

---
+++ azenia/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import header from './components/header.vue'
+
+</script>
+
+<template>
+  <header>
+   
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ azenia/src/components/header.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="">
+    {# <h1 class=""></h1> #}
+    
+  </div>
+</template>
+
+<style scoped>
+/* h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+} */
+</style>
